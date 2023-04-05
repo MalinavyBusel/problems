7231b7d08ff5c093d2bc4fdbcb9a8d37a99bfe34
Yahor Bagatyrevich <egorbgtrvch@gmail.com>
2023-04-05 14:14:36 +0300

    added tests for linked list problems

--- leetcode/83_remove_duplicates_from_sorted_L_list/main_test.go
+++ leetcode/83_remove_duplicates_from_sorted_L_list/main_test.go
@@ -1,7 +1,59 @@
 package _83_remove_duplicates_from_sorted_L_list
 
-//Input: head = [1,1,2]
-//Output: [1,2]
+import (
+	"reflect"
+	"testing"
+)
 
-//Input: head = [1,1,2,3,3]
-//Output: [1,2,3]
+func TestRemoveElements(t *testing.T) {
+	testCases := []struct {
+		name    string
+		message string
+		list    []int
+		want    []int
+	}{
+		{
+			name:    "case1",
+			message: "the func should return the list where each number occurs not more than once",
+			list:    []int{1, 1, 2},
+			want:    []int{1, 2},
+		},
+		{
+			name:    "case_empty_lists",
+			message: "the func should return the list where each number occurs not more than once",
+			list:    []int{1, 1, 2, 3, 3},
+			want:    []int{1, 2, 3},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			actual := deleteDuplicates(LinkedListFromSlice(test.list)).ToSlice()
+			if !reflect.DeepEqual(actual, test.want) {
+				t.Errorf("got: %q, want: %q", actual, test.want)
+			}
+		})
+
+	}
+}
+
+func LinkedListFromSlice(input []int) *ListNode {
+	dummy := new(ListNode)
+
+	cur := dummy
+	for _, n := range input {
+		next := &ListNode{Val: n}
+		cur, cur.Next = next, next
+	}
+
+	return dummy.Next
+}
+
+func (ln *ListNode) ToSlice() []int {
+	slice := make([]int, 0)
+	for cur := ln; cur != nil; cur = cur.Next {
+		slice = append(slice, cur.Val)
+	}
+
+	return slice
+}
--- leetcode/876_middle_of_the_linked_list/main_test.go
+++ leetcode/876_middle_of_the_linked_list/main_test.go
@@ -1,7 +1,59 @@
 package _876_middle_of_the_linked_list
 
-//Input: head = [1,2,3,4,5]
-//Output: [3,4,5]
+import (
+	"reflect"
+	"testing"
+)
 
-//Input: head = [1,2,3,4,5,6]
-//Output: [4,5,6]
+func TestRemoveElements(t *testing.T) {
+	testCases := []struct {
+		name    string
+		message string
+		list    []int
+		want    []int
+	}{
+		{
+			name:    "case1",
+			message: "the func should return the link to the middle node of the list",
+			list:    []int{1, 2, 3, 4, 5},
+			want:    []int{3, 4, 5},
+		},
+		{
+			name:    "case_even_nodes",
+			message: "if the amount of nodes is even, the func should return the one from the right",
+			list:    []int{1, 2, 3, 4, 5, 6},
+			want:    []int{4, 5, 6},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			actual := middleNode(LinkedListFromSlice(test.list)).ToSlice()
+			if !reflect.DeepEqual(actual, test.want) {
+				t.Errorf("got: %q, want: %q", actual, test.want)
+			}
+		})
+
+	}
+}
+
+func LinkedListFromSlice(input []int) *ListNode {
+	dummy := new(ListNode)
+
+	cur := dummy
+	for _, n := range input {
+		next := &ListNode{Val: n}
+		cur, cur.Next = next, next
+	}
+
+	return dummy.Next
+}
+
+func (ln *ListNode) ToSlice() []int {
+	slice := make([]int, 0)
+	for cur := ln; cur != nil; cur = cur.Next {
+		slice = append(slice, cur.Val)
+	}
+
+	return slice
+}
